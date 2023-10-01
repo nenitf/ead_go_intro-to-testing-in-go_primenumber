b64ef6f4dc5e35c26ecad76ba7577d1665e9ee3d
Neni <felipe.dacs@gmail.com>
2023-10-01 12:52:15 -0300

    :tv: add aula 5

--- main_test.go
+++ main_test.go
@@ -11,6 +11,9 @@ func Test_isPrime(t *testing.T) {
 	}{
 		{"prime", 7, true, "7 is prime number!"},
 		{"not prime", 8, false, "8 is not prime, because it is divisible by 2"},
+		{"zero", 0, false, "0 is not prime, by definition!"},
+		{"one", 1, false, "1 is not prime, by definition!"},
+		{"negative", -1, false, "Negative numbers are not prime, by definition!"},
 	}
 
 	for _, e := range primeTests {
